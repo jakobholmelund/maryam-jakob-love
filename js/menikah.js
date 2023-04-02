897f33afc2afd1bff4d839108b0d783460de5509
Jakob <jakobholmelund@gmail.com>
2023-04-02 15:09:59 +0200

    new stuff

--- js/menikah.js
+++ js/menikah.js
@@ -40,9 +40,9 @@ window.onscroll = function() {
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("toTop").style.display = "block";
+    //document.getElementById("toTop").style.display = "block";
   } else {
-    document.getElementById("toTop").style.display = "none";
+    //document.getElementById("toTop").style.display = "none";
   }
 }
 
@@ -50,6 +50,68 @@ function scrollFunction() {
 $(document).ready(function($) {
   $(".preloader-wrapper").fadeOut();
   $("body").removeClass("preloader-site");
+
+  $("#submitRSVPForm").on("click", function () {
+    const formData = $("#myForm").serializeArray();
+    const invite_code = $("#invite_code").val();
+    const NameRSVP = $("#NameRSVP").val();
+    const Email = $("#Email").val();
+    const Phone = $("#Phone").val();
+    const Comming = $('input[name="Comming"]:checked').val();
+    const NameList = $("#NameList").val();
+    const Camping = $("#Camping").val();
+
+    // Perform input validation if necessary
+
+    $.ajax({
+      url: "https://script.google.com/macros/s/AKfycbzwXACFxVzb0vIJlpkkc6eU4AnNrr5E4Ikrrtvf-KNSCrWhYJk-f2CO6Y0WlT38NTRz/exec",
+      method: "POST",
+      data: {
+        invite_code: invite_code,
+        NameRSVP: NameRSVP,
+        Email: Email,
+        Phone: Phone,
+        Comming: Comming,
+        NameList: NameList,
+        Camping: Camping,
+      },
+      success: function (response) {
+        const feedbackElement = $("#feedback");
+        feedbackElement.removeClass("is-hidden");
+
+        if (response.result === 'success') {
+          console.log("Form submitted successfully");
+          feedbackElement.addClass("is-success");
+          feedbackElement.text("Form submitted successfully"); // Or use response.message if it's available in the success response
+        } else {
+          console.error("Error submitting form");
+          feedbackElement.addClass("is-danger");
+          feedbackElement.text(response.message);
+        }
+
+        setTimeout(function () {
+          feedbackElement.addClass("is-hidden");
+        }, 5000);
+      },
+      error: function (error) {
+        console.error("Error submitting form", error);
+        const feedbackElement = $("#feedback");
+        feedbackElement.removeClass("is-hidden");
+      
+        // Customize the feedback element appearance based on the API response
+        feedbackElement.addClass("is-danger"); // Add a class for error feedback
+        const errorMessage = error.responseJSON && error.responseJSON.message
+          ? error.responseJSON.message
+          : "An error occurred. Please try again.";
+        feedbackElement.text(errorMessage); // Update the feedback text with the message from the API or a default error message
+      
+        // Optionally, hide the feedback element after a certain time
+        setTimeout(function () {
+          feedbackElement.addClass("is-hidden");
+        }, 5000);
+      },
+    });
+  });
 });
 $(window).on("load", function() {
   var Body = $("body");
